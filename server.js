78c4118ba70706ab235fe5b6179a3d28ac07be54
RehanaDelair <rehanad10@gmail.com>
2017-08-10 19:57:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,65 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article One | Rehana Delair',
+    heading:'Article One',
+    date:'August 10, 2017',
+    content:`<p>
+                This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) 
+            </p>
+            <p>
+                This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) 
+            </p>
+            <p>
+                This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) This is my first article. I am just a lazy person. So I will copy this lines. What I am blabbering about. ;) 
+            </p>`
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+    var htmlTemplate=`
+        <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width-device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                   ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`;
+    
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
