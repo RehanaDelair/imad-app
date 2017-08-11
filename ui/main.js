c98113583c682302da5d0dbe30708c648e626187
RehanaDelair <rehanad10@gmail.com>
2017-08-11 09:12:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
 
+//change to content of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+//move the image
+var img=document.getElementById('madi');
+img.onClick= function (){
+    img.style.marginleft = '100px';
+};
