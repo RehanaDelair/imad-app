556cff065d7408d178e6655bd7e8d530f3ab1ae8
RehanaDelair <rehanad10@gmail.com>
2017-08-13 10:00:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 //Counter code
-var button=document.getElementById("counter");
+var button=document.getElementById('counter');
 
 button.onclick = function() {
     
@@ -13,7 +13,7 @@ button.onclick = function() {
           //Take some action
           if (request.status === 200) {
               var counter = request.responseText;
-              var span = document.getElementById("count");
+              var span = document.getElementById('count');
               span.innerHTML = counter.toString(); 
           }
       }
@@ -21,7 +21,7 @@ button.onclick = function() {
     };
     
     //Make the request
-    request.open('GET', "http://rehanad10.imad.hasura-app.io/counter", true);
-    request.send(null);
+    request.open('GET', 'http://rehanad10.imad.hasura-app.io/counter', true);
+    request.send();
     
 };
