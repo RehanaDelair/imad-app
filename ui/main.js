ac7c59bc169578aa7c2c19366900d8fd4e0b036f
RehanaDelair <rehanad10@gmail.com>
2017-08-14 15:42:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,20 @@ button.onclick = function() {
     request.send();
     
 };
+
+//submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    // Make a request to the server and send name
+    
+    //Capture a list of names and render it as a list
+    var names = ["Name 1", "Name 2", "Name 3"];
+    var list = "";
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('name_list');
+    ul.innerHTML = list;
+}
