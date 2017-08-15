f32f878c82e4ef649eae4adaec38d49f0b3b5262
RehanaDelair <rehanad10@gmail.com>
2017-08-15 09:49:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,6 @@ button.onclick = function() {
 };
 
 //submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -53,6 +51,8 @@ submit.onclick = function() {
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://rehanad10.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send();
     
