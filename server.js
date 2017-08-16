900e8e42e197849438c14584f7d336058653124b
RehanaDelair <rehanad10@gmail.com>
2017-08-16 23:54:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,10 +119,8 @@ app.get('/submit-name', function (req, res) { // URL: /submit-name?name=xxx
 });
 
 app.get('/articles/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-    
      
-    pool.query("SELECT * FROM article WHERE title='"+ articleName + "'", function (err, result) {
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName] , function (err, result) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
