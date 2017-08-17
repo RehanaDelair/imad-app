9929b67044a637fad54004968a1a08da57ad7a58
RehanaDelair <rehanad10@gmail.com>
2017-08-17 14:01:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,4 @@
-//Counter code
-var button=document.getElementById('counter');
-
-button.onclick = function() {
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function (){
-      if (request.readyState === XMLHttpRequest.DONE){
-          //Take some action
-          if (request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString(); 
-          }
-      }
-      //Not done yet
-    };
-    
-    //Make the request
-    request.open('GET', 'http://rehanad10.imad.hasura-app.io/counter', true);
-    request.send();
-    
-};
-
-//submit Name
+//submit username, password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -51,9 +24,10 @@ submit.onclick = function() {
     };
     
     //Make the request
-    var nameInput = document.getElementById('name');
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
     var name = nameInput.value;
-    request.open('GET', 'http://rehanad10.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send();
+    request.open('POST', 'http://rehanad10.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(JSON.stringify({username: username, password: password}));
     
 };
