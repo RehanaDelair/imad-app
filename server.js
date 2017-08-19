68702cd92e0980f1c2523310f464d937c737d2f2
RehanaDelair <rehanad10@gmail.com>
2017-08-19 10:57:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,7 @@ app.post('/create-user', function (req, res) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
+            console.log(result.rowCount);
             if(result.rowCount > 0){
                 res.send("Username already taken. Choose another Username");
             }else if(result.rowCount === 0){
