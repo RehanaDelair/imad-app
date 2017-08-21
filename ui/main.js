e5c51f803260c79b8b7bb9fc68d0f87a91fa8b06
RehanaDelair <rehanad10@gmail.com>
2017-08-21 18:22:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,28 +44,8 @@ function loadLogin() {
                 <br><br>
                 <input type='submit' value='Login' id='login_btn'/>
                 <input type='submit' value='SignUp' id='register_btn'/>`;
-}
-
-function checkLogin() {
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function (){
-          if (request.readyState === XMLHttpRequest.DONE){
-              //Take some action
-              if (request.status === 200) {
-                  loadLoggedInUser(request.responseText);
-              } else {
-                  loadLogin();
-              }
-          }
-        };
-        
-    request.open('GET', '/check-login', true);
-    request.send();
-}
-
-
-//submit username, password to login
+                
+                //submit username, password to login
 var login = document.getElementById('login_btn');
 login.onclick = function() {
     
@@ -128,6 +108,25 @@ register.onclick = function() {
     request.send(JSON.stringify({username: username, password: password}));
     
 };
+}
+
+function checkLogin() {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function (){
+          if (request.readyState === XMLHttpRequest.DONE){
+              //Take some action
+              if (request.status === 200) {
+                  loadLoggedInUser(request.responseText);
+              } else {
+                  loadLogin();
+              }
+          }
+        };
+        
+    request.open('GET', '/check-login', true);
+    request.send();
+}
 
 //check if user is logged in
 checkLogin();
