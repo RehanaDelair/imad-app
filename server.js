20253494433af6477b01dd7d406c01d1db40fed6
RehanaDelair <rehanad10@gmail.com>
2017-08-23 13:11:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.post('/login', function (req, res) {
             }else{
                 //Match the password
                 var dbString = result.rows[0].password;
-                var salt = dbString.split("$")[2];
+                var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password, salt);
                 if(hashedPassword == dbString){
                     
