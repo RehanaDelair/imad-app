6f2d403b0f72dc2d701e053b99a6cca993103b1c
RehanaDelair <rehanad10@gmail.com>
2017-08-24 21:38:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,11 +171,11 @@ app.get('/logout', function (req, res) {
     res.send("You are logged out!");
 });
 
-var Pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     //make a select request
     //return a response with the results
-    Pool.query("SELECT * from test", function(err, result) {
+    pool.query("SELECT * from test", function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
