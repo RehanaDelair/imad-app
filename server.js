c4ef5250420fb95c73123617304c0a3209f5d249
RehanaDelair <rehanad10@gmail.com>
2017-08-28 10:40:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ app.post('/create-user', function (req, res) {
             }else if(count === 0){
                 var salt = crypto.randomBytes(128).toString('hex');
                 var dbString= hash(password, salt);
-                pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result){
+                pool.query('INSERT INTO "user"(username, password) VALUES($1, $2)', [username, dbString], function (err, result){
                     if(err) {
                         res.status(500).send(err.toString());
                     } else {
